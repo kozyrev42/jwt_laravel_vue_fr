d1a977e4303bb829ac8a4badf542a992c2f2cba8
kozyrev42 <alexander.kozyrev@pluscards.ru>
2023-08-02 11:40:41 +0700

    при логировании на сервере, возвращаемый 'access_token' поместил в 'localStorage'

--- src/components/User/UserLogin.vue
+++ src/components/User/UserLogin.vue
@@ -7,9 +7,9 @@
     <router-link to="/" class="btn btn-outline-success mb-2">Home</router-link><br>
 
     <div class="w-25">
-      <input type="email" id="email" class="form-control mt-3 mb-3" placeholder="email">
-      <input type="password" id="password" class="form-control mt-3 mb-3" placeholder="password">
-      <input type="submit" class="btn btn-primary">
+      <input v-model="email" type="email" id="email" class="form-control mt-3 mb-3" placeholder="email">
+      <input v-model="password" type="password" id="password" class="form-control mt-3 mb-3" placeholder="password">
+      <input @click.prevent="login" type="submit" class="btn btn-primary">
     </div>
 
   </div>
@@ -17,7 +17,33 @@
 
 <script>
 export default {
-  name: "UserLogin"
+  name: "UserLogin",
+
+  data() {
+    return {
+      email: null,
+      password: null
+    }
+  },
+
+  methods: {
+    login() {
+      this.$axios
+        .post(`${process.env.VUE_APP_BASE_URL}/api/auth/login`,
+          {
+            email: this.email,
+            password: this.password
+          })
+        .then(response => {
+          // помещаем токен в localStorage
+          localStorage.setItem('jwt3_access_token', response.data.access_token);
+        })
+
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  }
 }
 </script>
 
