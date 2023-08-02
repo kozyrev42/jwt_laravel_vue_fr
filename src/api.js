c4dcd9ab6f3a3d5ac196e8788c067c2ed4c560ef
kozyrev42 <alexander.kozyrev@pluscards.ru>
2023-08-02 23:52:18 +0700

    добавил обработку ошибок, если токен не валидный, то перенаправляет на страницу логина.

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import router from "@/router";
 
 // Создаем экземпляр axios
 const api = axios.create({
@@ -15,6 +16,23 @@ api.interceptors.request.use(
         return config;
     },
     error => {
+        return Promise.reject(error);
+    }
+);
+
+// Добавляем интерцептор ответов
+api.interceptors.response.use(
+    response => {
+        return response;
+    },
+    error => {
+        console.log('Interceptor response error:', error);
+
+        // Если статус ответа 401 (Unauthorized), перенаправляем на страницу логина
+        if (error.response && error.response.status === 401) {
+            router.push({ name: 'user.login' });
+        }
+
         return Promise.reject(error);
     }
 );
