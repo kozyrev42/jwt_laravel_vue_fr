4c6f46ab45ee13459cf16cd0b575acf74b1725ef
kozyrev42 <alexander.kozyrev@pluscards.ru>
2023-08-03 14:09:43 +0700

    Спрятал кнопки, от неавторизованных пользователей. Обновляем компонент при выходе.

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <div class="badge bg-primary text-wrap">HomeComponent</div> <br/><br/>
-    <router-link to="/fruits" class="btn btn-outline-primary mb-2">Фрукты</router-link><br/><br/>
 
-    <router-link to="/user/login" class="btn btn-success">Login</router-link><br/><br/>
-    <router-link to="/user/registration" class="btn btn-primary">Registration</router-link><br/><br/>
+    <router-link to="/fruits" class="btn btn-outline-primary mb-2">Фрукты</router-link><br/><br/>
+    <router-link v-if="!accessToken" to="/user/login" class="btn btn-success">Login</router-link><br/><br/>
+    <router-link v-if="!accessToken" to="/user/registration" class="btn btn-primary">Registration</router-link><br/><br/>
 
     <button @click.prevent="logout" class="btn btn-danger">Выход</button>
   </div>
@@ -15,15 +15,35 @@ import api from "@/api";
 
 export default {
   name: 'HomeComponent',
+
+  data() {
+    return {
+      accessToken: null
+    }
+  },
   props: {
     msg: String
   },
 
   mounted() {
     this.testApi();
+    this.getAccessToken();
   },
 
+
+  // обновляем статус токена после обновления компонента
+  updated() {
+    this.getAccessToken();
+  },
+
+
   methods: {
+    // получение токена, из локального хранилища
+    getAccessToken() {
+      this.accessToken = localStorage.getItem('jwt3_access_token');
+    },
+
+
     testApi() {
       api.get('/api/test')
         .then(response => {
@@ -44,6 +64,9 @@ export default {
           console.log(response)
           // удаляем токен из localStorage
           localStorage.removeItem('jwt3_access_token');
+
+          // обновляем компонент
+          this.$forceUpdate();
         })
 
         .catch(error => {
